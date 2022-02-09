453d2caa12c0115c856e431f180b582586f73dcc
akhazee <devarttidez@gmail.com>
2022-02-09 16:43:32 -0500

    Simple markup for GUI

--- CrapsGUITester.py
+++ CrapsGUITester.py
@@ -1,6 +1,15 @@
-#This is a testing file for creating a GUI for CrapsTable.py
+from tkinter import *
+window=Tk()
 
-import tkinter
-top = tkinter.tk()
-#Code
-top.mainloop()
+lbl=Label(window, text="Craps table game", fg='red', font=("Helvetica", 16))
+lbl.place(x=320, y=50)
+
+btn=Button(window, text="Start game", fg='blue')
+btn.place(x=80, y=100)
+
+txtfld=Entry(window, text="How much would you like to bet?", bd=5)
+txtfld.place(x=80, y=150)
+
+window.title('Craps Table game')
+window.geometry("800x400+10+10")
+window.mainloop()
